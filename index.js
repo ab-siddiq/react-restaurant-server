ac4d3a5028a7b85d258c1ee2f88a290dc9f20f67
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2024-01-10 00:02:05 +0600

    create api for reviews data and fetch from database

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");    
-const app = express(); 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
@@ -10,7 +10,6 @@ app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("server is running");
 });
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gjnzlrq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,6 +33,11 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -45,7 +49,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
